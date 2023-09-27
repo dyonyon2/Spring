081c969af2f66e939003806921b6224c3f53be2c
jychoi9712 <jychoi9712@naver.com>
2023-09-27 11:13:40 +0900

    2023-09-27 "Spring Framework Master Class Finished"

--- web/src/main/java/com/in28minutes/jee/LoginService.java
+++ web/src/main/java/com/in28minutes/jee/LoginService.java
@@ -1,5 +1,8 @@
 package com.in28minutes.jee;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class LoginService {
 	
 	public boolean isUserValid(String user, String password) {
--- web/src/main/java/com/in28minutes/springmvc/LoginController.java
+++ web/src/main/java/com/in28minutes/springmvc/LoginController.java
@@ -1,18 +1,19 @@
 package com.in28minutes.springmvc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.in28minutes.jee.LoginService;
 
 @Controller
 public class LoginController {
 	
-	LoginService service = new LoginService();
+	@Autowired
+	LoginService service;
 
 	@RequestMapping(value="/login", method=RequestMethod.GET)
 	//@ResponseBody //Dispatcher Controller가 아니라 직접 응답을 return할 때!
